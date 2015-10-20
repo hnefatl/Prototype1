262dfe5c87ed7912dea5330a99177f7cca339756
Keith <hnefatl@gmail.com>
2015-10-20 17:58:47 +0100

    Started work on the Admin window

---
+++ Client/Admin/AdminWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+
+using Data.Models;
+
+namespace Client.Admin
+{
+    public partial class AdminWindow
+        : Window, INotifyPropertyChanged
+    {
+        protected ObservableCollection<Checkable<Room>> _Rooms = new ObservableCollection<Checkable<Room>>();
+        public ObservableCollection<Checkable<Room>> Rooms
+        {
+            get { return _Rooms; }
+            set { _Rooms = value; OnPropertyChanged("Rooms"); }
+        }
+        public List<Room> SelectedRooms { get { return Rooms.Where(c => c.Checked).Select(c => c.Value).ToList(); } }
+
+        public AdminWindow()
+        {
+            InitializeComponent();
+
+            using (DataRepository Repo = new DataRepository())
+            {
+
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string PropertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(PropertyName));
+        }
+    }
+}
--- Client/Checkable.cs
+++ Client/Checkable.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.ComponentModel;
 
-namespace Client.TimetableDisplay
+namespace Client
 {
     public class Checkable<T>
         : INotifyPropertyChanged
