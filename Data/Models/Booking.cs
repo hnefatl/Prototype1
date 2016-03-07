a5de4b2ba609c4a13e5d1d3d8f9db330e8a008ea
hnefatl <hnefatl@gmail.com>
2016-03-07 09:47:13 +0000

    Backup

--- Data/Models/Booking.cs
+++ Data/Models/Booking.cs
@@ -39,7 +39,9 @@ namespace Data.Models
         // The subject of this booking
         public virtual Subject Subject { get; set; }
 
+        // Students attending this booking
         public virtual List<Student> Students { get; set; }
+        // Teacher who made the booking
         public virtual Teacher Teacher { get; set; }
 
         public Booking()
@@ -56,20 +58,23 @@ namespace Data.Models
             this.BookingType = BookingType;
         }
 
+        // Determines if this booking will happen on a given day
         public bool MatchesDay(DateTime Day)
         {
             return (BookingType == BookingType.Single && Day.Date == Date) ||
                     (BookingType == BookingType.Weekly && (Day.Date - Date).Days % 7 == 0) ||
                     (BookingType == BookingType.Fortnightly && (Day.Date - Date).Days % 14 == 0) ||
-                    (BookingType == BookingType.Monthly && (Day.Date - Date).Days % 31 == 0);
+                    (BookingType == BookingType.Monthly && (Day.Date - Date).Days % 28 == 0);
         }
         public override bool Conflicts(List<DataModel> AllBookings)
         {
+            // Checks for conflicts between booking objects
             return AllBookings.Cast<Booking>().Any(b => b.Id != Id && b.Date == Date && b.TimeSlot == TimeSlot && b.Rooms.Intersect(Rooms).Count() != 0);
         }
 
         public override void Update(DataModel Other)
         {
+            // Copy data from other item into this
             Booking b = (Booking)Other;
             Ticks = b.Ticks;
             BookingType = b.BookingType;
@@ -81,6 +86,7 @@ namespace Data.Models
             Teacher = b.Teacher;
         }
 
+        // Serialise to an output stream
         public override void Serialise(Writer Out)
         {
             base.Serialise(Out);
@@ -97,6 +103,7 @@ namespace Data.Models
                 Out.Write(s.Id);
             Out.Write(Teacher.Id);
         }
+        // Deserialise from an input stream
         protected override void Deserialise(Reader In)
         {
             base.Deserialise(In);
@@ -113,6 +120,7 @@ namespace Data.Models
                 Students.Add(new Student() { Id = In.ReadInt32() });
             Teacher = new Teacher() { Id = In.ReadInt32() };
         }
+        // Obtain references to related objects
         public override bool Expand(IDataRepository Repo)
         {
             try
@@ -131,6 +139,7 @@ namespace Data.Models
             }
             return true;
         }
+        // Set references to this object
         public override void Attach()
         {
             if (TimeSlot != null)
@@ -142,6 +151,7 @@ namespace Data.Models
             if (Teacher != null)
                 Teacher.Bookings.Add(this);
         }
+        // Remove references to this object
         public override void Detach()
         {
             if (TimeSlot != null)
@@ -154,18 +164,16 @@ namespace Data.Models
                 Teacher.Bookings.RemoveAll(i => i.Id == Id);
         }
 
+        // Equality operators
         public override bool Equals(object obj)
         {
-            if (!(obj is Booking))
+            if (!(obj is Booking)) // Type check
                 return false;
-
-            Booking b = (obj as Booking);
-            return b == null || ReferenceEquals(this, obj) || (b.BookingType == BookingType && b.Date == Date && b.Id == Id &&
-                b.Rooms == Rooms && b.Students == Students && b.Subject == Subject && b.Teacher == Teacher && b.Ticks == Ticks &&
-                b.TimeSlot == TimeSlot);
+            return this == (obj as Booking);
         }
         public static bool operator ==(Booking One, Booking Two)
         {
+            // Check for references
             if (ReferenceEquals(One, null))
             {
                 if (ReferenceEquals(Two, null))
@@ -175,6 +183,7 @@ namespace Data.Models
             if (ReferenceEquals(Two, null))
                 return false;
 
+            // Check for references then compare fields
             return ReferenceEquals(One, Two) || (One.BookingType == Two.BookingType && One.Date == Two.Date && One.Id == Two.Id &&
                 One.Rooms == Two.Rooms && One.Students == Two.Students && One.Subject == Two.Subject && One.Teacher == Two.Teacher && One.Ticks == Two.Ticks &&
                 One.TimeSlot == Two.TimeSlot);
@@ -189,6 +198,7 @@ namespace Data.Models
         }
     }
 
+    // Recurrence type of the booking
     public enum BookingType
     {
         // A one off booking
